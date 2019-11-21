dc407341f38e68c0ce88551699294eccfc8a6043
Anton Ustinoff <a.a.ustinoff@gmail.com>
2019-11-21 12:14:55 +0400

    update crm

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.925959261fa5effb3a3f314c8a335848.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.db66c47552a24565f3485e843708082b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('install', e => {
     e.waitUntil(
